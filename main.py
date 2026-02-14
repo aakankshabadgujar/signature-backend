405e8a5934773389063cc4dda9e5a3cc0dfe101b
aakankshabadgujar <badgujaraakanksha1915@gmail,com>
2026-02-14 22:34:32 +0530

    fixed CORS for vercel frontend

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://signature-frontend-one.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
